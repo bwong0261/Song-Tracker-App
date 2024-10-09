ab8d9976f6ded022f56ef395ed1a9ba206ec88eb
Benjamin Wong <benjamin02615@gmail.com>
2024-10-09 13:31:43 -0700

    Added Specifications for Constructor and Fields

--- src/main/model/Song.java
+++ src/main/model/Song.java
@@ -1,22 +1,34 @@
 package model;
 
+// Represents a song that has title, instrument, learned, favourite.
 public class Song {
-    private String title;
-    private String instrument;
-    private Boolean learned;
-    private Boolean favourite;
+    private String title; // title of song
+    private String artist; // artist of song
+    private String instrument; // instrument of song to learn
+    private Boolean learned; // is song learned or not?
+    private Boolean favourite; // is song favourited?
     
-
-    public Song(String title, String instrument, Boolean learned, Boolean favourite){
+    /*
+     * REQUIRES: title has non-zero length, artist has non-zero length,
+     * instrument has non-zero length,
+     * EFFECTS: title of song set to title, artist of song set to artist,
+     * instrument of song set to instrument, learned initially set to false,
+     * favourite initially set to false
+     */
+    public Song(String title, String artist, String instrument){
         this.title = title;
+        this.artist = artist;
         this.instrument = instrument;
         this.learned = false;
-        this.favourite = favourite;
-
+        this.favourite = false;
     }
 
     public String getTitle() {
         return title;
+    }
+
+    public String getArtist() {
+        return artist;
     }
 
     public String getInstrument() {
@@ -29,6 +41,14 @@ public class Song {
 
     public Boolean favourite() {
         return favourite;
+    }
+
+    public Boolean makeLearned(){
+        return learned = true;
+    }
+
+    public Boolean makeFavourite(){
+        return favourite = true;
     }
 
 
