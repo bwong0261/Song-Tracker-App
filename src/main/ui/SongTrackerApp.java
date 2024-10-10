1e852a670e60507e464364dd9a0ac9ed37b6f7ad
Benjamin Wong <benjamin02615@gmail.com>
2024-10-09 23:34:47 -0700

    Updated Specification and Implementation for SongTrackerApp Class

--- src/main/ui/SongTrackerApp.java
+++ src/main/ui/SongTrackerApp.java
@@ -13,6 +13,7 @@ public class SongTrackerApp {
     private SongsLearning songsLearning;
     private SongsLearned songsLearned;
     private Scanner input;
+    
 
     // EFFECTS: starts runSongTracker()
     public SongTrackerApp(){
@@ -61,8 +62,45 @@ public class SongTrackerApp {
     // EFFECTS: Prints all of the songs in SongsToLearn
     // and gives user option to add or remove song 
     private void viewSongsToLearn() {
-        // TODO STUB
+        boolean inSongsToLearn = true;
+        while(inSongsToLearn){
+            System.out.println("\n--- Songs To Learn ---");
+            System.out.println("\t a -> add a song");
+            System.out.println("\t b -> remove a song");
+            System.out.println("\t q -> quit");
+            if ((songsToLearn.getSongs()).isEmpty()){
+                System.out.println("You have no songs on your learn list");
+            } else {
+                for(Song song: songsToLearn.getSongs()){
+                    System.out.println("\n\nTitle:" + song.getTitle() +
+                                       "\nArtist:" + song.getArtist() +
+                                       "\nInstrument:" + song.getInstrument());
+                }
+            }
+            String command = input.next();
+            command = command.toLowerCase();
+            if(command.equals("a")){
+                addSongToLearn();
+            } else if (command.equals("b")){
+                // removeSongToLearn();
+            } else if (command.equals("q")){
+                inSongsToLearn = false;
+            } else {
+                System.out.println("Not valid choice");
+            }}}
+
+    private void addSongToLearn(){
+        System.out.println("Enter a Title");
+        String title = input.next();
+        System.out.println("Enter an Artist");
+        String artist = input.next();
+        System.out.println("Enter an Instrument");
+        String instrument = input.next();
+        Song newSong = new Song(title, artist, instrument);
+        songsToLearn.addSongToSongsToLearn(newSong);
+
     }
+
 
     // EFFECTS: Prints all of the songs in SongsLearning
     // and gives user option to add or remove song 
@@ -87,6 +125,7 @@ public class SongTrackerApp {
 
     // EFFECTS: Initialized all of our lists to start and be empty
     private void initialize(){
+        input = new Scanner(System.in);
         songsToLearn = new SongsToLearn();
         songsLearning = new SongsLearning();
         songsLearned = new SongsLearned();
