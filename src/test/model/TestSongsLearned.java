e64123c8f93b3aea6ac9d2e30e782b74a117b3da
Benjamin Wong <benjamin02615@gmail.com>
2024-10-09 18:20:19 -0700

    Tests for SongsLearned Class

---
+++ src/test/model/TestSongsLearned.java
@@ -0,0 +1,54 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestSongsLearned {
+    private SongsLearned testSongsLearned;
+    private Song s1;
+    private Song s2;
+
+    
+    @BeforeEach
+    void runBefore() {
+        testSongsLearned = new SongsLearned();
+        Song s1 = new Song("Test", "Me", "Guitar");
+        Song s2 = new Song("Okay", "You", "Piano");
+
+    }
+
+    @Test
+    void testAddOneSong(){
+        testSongsLearned.addSongToSongsLearned(s1);
+        ArrayList<Song> songs = testSongsLearned.getSongs();
+        assertEquals(s1, songs.get(0));
+        assertEquals(1, songs.size());
+
+    }
+
+    @Test
+    void testAddTwoSongs(){
+        testSongsLearned.addSongToSongsLearned(s1);
+        testSongsLearned.addSongToSongsLearned(s2);
+        ArrayList<Song> songs = testSongsLearned.getSongs();
+        assertEquals(s1, songs.get(0));
+        assertEquals(s2, songs.get(1));
+        assertEquals(2, songs.size());
+
+    }
+
+    @Test
+    void RemoveSong(){
+        testSongsLearned.addSongToSongsLearned(s1);
+        testSongsLearned.addSongToSongsLearned(s2);
+        testSongsLearned.removeSongToSongsLearned(s1);
+        ArrayList<Song> songs = testSongsLearned.getSongs();
+        assertEquals(s2, songs.get(0));
+        assertEquals(1, songs.size());
+
+    }
+}
