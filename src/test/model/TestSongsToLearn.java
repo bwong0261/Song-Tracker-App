7986eba43791798745c2703c7ba1d7f218d2a96a
Benjamin Wong <benjamin02615@gmail.com>
2024-10-09 17:47:35 -0700

    Tests for SongsToLearn

---
+++ src/test/model/TestSongsToLearn.java
@@ -0,0 +1,43 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestSongsToLearn {
+    private SongsToLearn testSongsToLearn;
+    private Song s1;
+    private Song s2;
+
+    
+    @BeforeEach
+    void runBefore() {
+        testSongsToLearn = new SongsToLearn();
+        Song s1 = new Song("Test", "Me", "Guitar");
+        Song s2 = new Song("Okay", "You", "Piano");
+
+    }
+
+    @Test
+    void testAddOneSong(){
+        testSongsToLearn.addSongToSongsToLearn(s1);
+        ArrayList<Song> songs = testSongsToLearn.getSongs();
+        assertEquals(s1, songs.get(0));
+        assertEquals(1, songs.size());
+
+    }
+
+    @Test
+    void testAddTwoSongs(){
+        testSongsToLearn.addSongToSongsToLearn(s1);
+        testSongsToLearn.addSongToSongsToLearn(s2);
+        ArrayList<Song> songs = testSongsToLearn.getSongs();
+        assertEquals(s1, songs.get(0));
+        assertEquals(s2, songs.get(1));
+        assertEquals(2, songs.size());
+
+    }
+}
