ac376a896113e0aa5b2be4e21d47c4ade61b8095
Benjamin Wong <benjamin02615@gmail.com>
2024-10-09 18:18:31 -0700

    Specification for SongsLearned Class

---
+++ src/main/model/SongsLearned.java
@@ -0,0 +1,35 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SongsLearned {
+    private ArrayList<Song> songsLearned;
+
+    /** 
+     * EFFECTS: SongsLearned is initially set to empty.
+     */
+    public SongsLearned(){
+        songsLearned = new ArrayList<>();
+
+        }
+
+    // EFFECTS: Adds song to songsLearned
+    public void addSongToSongsLearned(Song song){
+        // STUB
+    }
+
+    // REQUIRES: songsLearned must be non-empty
+    // EFFECTS: Removes song to songsLearned
+    public void removeSongToSongsLearned(Song song){
+        // STUB
+    }
+
+    public ArrayList<Song> getSongs(){
+        // STUB
+    }
+
+
+    }
+
+
